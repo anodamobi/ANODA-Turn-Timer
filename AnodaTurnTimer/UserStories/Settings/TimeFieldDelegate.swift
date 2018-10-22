0a9b68b448d3031204f83571b80e26e62d439b38
SergLam <lamcevs0@gmail.com>
2018-10-22 12:10:09 +0300

    Refactor text field delegate

--- AnodaTurnTimer/UserStories/Settings/TimeFieldDelegate.swift
+++ AnodaTurnTimer/UserStories/Settings/TimeFieldDelegate.swift
@@ -11,56 +11,10 @@ import UIKit
 extension SettingsVC: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        // Add restcirtion to ditits
-        let decimalSet = NSCharacterSet(charactersIn: "0123456789:" )
-        let characterSet = CharacterSet(charactersIn: string)
-        let isDecimal = decimalSet.isSuperset(of: characterSet)
-        // Add restriction to 5 symbols
-        let text = textField.text ?? ""
-        let newLength = text.count + string.count - range.length
-        let isInRange = newLength <= 5
-        // Add colon separator
-        switch range.location {
-        case 0:
-            if(text.contains(":")) {
-                textField.text = ""
-            }
-        case 2:
-            if(text.contains(":")) {
-                textField.text = text
-                moveCursor(textField: textField, offset: 1)
-            } else {
-                textField.text = text + ":"
-            }
-        case 3:
-            if(string == ""){
-                if(text.contains(":")){
-                    moveCursor(textField: textField, offset: -1)
-                    let newText = String(text.dropLast()) + ":"
-                    textField.text = newText
-                }
-            }
-        case 4:
-            break
-        default:
-            break
-        }
-        // Add restriction to 60 minutes and 60 seconds
-        // First and third symbol - only 0-5
-        // Second and fourth symbol - all allowed
-        let validTimeSet = NSCharacterSet(charactersIn: "012345" )
-        var isValidTime: Bool = true
-        let charSet = CharacterSet(charactersIn: string)
-        switch range.location {
-        case 0:
-            isValidTime = validTimeSet.isSuperset(of: charSet)
-        case 2:
-            isValidTime = validTimeSet.isSuperset(of: charSet)
-        case 3:
-            isValidTime = validTimeSet.isSuperset(of: charSet)
-        default:
-            break
-        }
+        let isDecimal = checkDecimalDigits(newString: string)
+        let isInRange = checkTextMaxCount(textFieldText: textField.text ?? "", newString: string, atRange: range, maxCount: 5)
+        addColonSeparator(textField: textField, newString: string, atRange: range)
+        let isValidTime: Bool = checkMinutesAndSeconds(newString: string, rangeLocation: range.location)
         return isDecimal && isInRange && isValidTime
     }
     
@@ -75,11 +29,74 @@ extension SettingsVC: UITextFieldDelegate {
         return String.timeString(time: TimeInterval(time))
     }
     
-    func moveCursor(textField: UITextField, offset: Int){
+    func moveCursor(textField: UITextField, offset: Int) {
         if let selectedRange = textField.selectedTextRange {
             if let newPosition = textField.position(from: selectedRange.start, offset: offset) {
                 textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
             }
         }
+    }
+    
+    // MARK: Text field validation methods
+    
+    func checkTextMaxCount(textFieldText: String, newString: String, atRange: NSRange, maxCount: Int) -> Bool {
+        // Add restriction to 5 symbols
+        let newLength = textFieldText.count + newString.count - atRange.length
+        return newLength <= maxCount
+    }
+    
+    func checkDecimalDigits(newString: String) -> Bool {
+        // Add restcirtion to ditits
+        let decimalSet = NSCharacterSet(charactersIn: "0123456789:")
+        let characterSet = CharacterSet(charactersIn: newString)
+        return decimalSet.isSuperset(of: characterSet)
+    }
+    
+    func addColonSeparator(textField: UITextField, newString: String, atRange: NSRange){
+        let text = textField.text ?? ""
+        // Add colon separator
+        switch atRange.location {
+        case 0:
+            if(text.contains(":")) {
+                textField.text = ""
+            }
+        case 2:
+            if(text.contains(":")) {
+                textField.text = text
+                moveCursor(textField: textField, offset: 1)
+            } else {
+                textField.text = text + ":"
+            }
+        case 3:
+            if newString.isEmpty, text.contains(":") {
+                moveCursor(textField: textField, offset: -1)
+                let newText = String(text.dropLast()) + ":"
+                textField.text = newText
+            }
+        case 4:
+            break
+        default:
+            break
+        }
+    }
+    
+    func checkMinutesAndSeconds(newString: String, rangeLocation: Int) -> Bool {
+        // Add restriction to 60 minutes and 60 seconds
+        // First and third symbol - only 0-5
+        // Second and fourth symbol - all allowed
+        let validTimeSet = NSCharacterSet(charactersIn: "012345" )
+        var isValidTime: Bool = true
+        let charSet = CharacterSet(charactersIn: newString)
+        switch rangeLocation {
+        case 0:
+            isValidTime = validTimeSet.isSuperset(of: charSet)
+        case 2:
+            isValidTime = validTimeSet.isSuperset(of: charSet)
+        case 3:
+            isValidTime = validTimeSet.isSuperset(of: charSet)
+        default:
+            break
+        }
+        return isValidTime
     }
 }
